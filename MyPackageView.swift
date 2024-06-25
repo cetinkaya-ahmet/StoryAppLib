a8c86836f5b6c858feb5d6bbf333acc7dacd4cfc
ahmetcetinkaya <4hmetcetinkaya@gmail.com>
2024-06-25 16:57:52 +0300

    Initial Commit

---
+++ MyPackageView.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  StoryAppLib
+//
+//  Created by Ahmet Çetinkaya on 25.06.2024.
+//
+
+import Foundation
+import SwiftUI
+
+public struct MyPackageView: View {
+    public init() {}
+    public var body: some View {
+        Text("Hello world in MyPackage!")
+    }
+}
+
