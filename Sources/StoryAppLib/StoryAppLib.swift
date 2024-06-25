1077dbf8f45b9fdefeb5a07e9cfdc9c6e810c1af
ahmetcetinkaya <4hmetcetinkaya@gmail.com>
2024-06-25 17:08:26 +0300

    test

--- Sources/StoryAppLib/StoryAppLib.swift
+++ Sources/StoryAppLib/StoryAppLib.swift
@@ -1,2 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+import SwiftUI
+
+public struct StoryAppLibView2: View {
+    public init() {}
+    public var body: some View {
+        Text("Hello world in MyPackage!")
+    }
+}
